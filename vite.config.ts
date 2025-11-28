f04c6ad07ab873e2ee72e573c4d785f5847bc61c
xuejiantao <jiantaoxue@huanle.com>
2025-11-28 14:42:00 +0800

    feat: 添加API_KEY

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -13,7 +13,9 @@ export default defineConfig(({ mode }) => {
     base: './',
     define: {
       // Maps import.meta.env.VITE_API_KEY to the VITE_API_KEY or API_KEY environment variable
-      'import.meta.env.VITE_API_KEY': JSON.stringify(env.VITE_API_KEY || env.API_KEY)
-    }
+      'import.meta.env.VITE_API_KEY': JSON.stringify(
+        env.VITE_API_KEY || env.API_KEY
+      ),
+    },
   };
-})
+});
